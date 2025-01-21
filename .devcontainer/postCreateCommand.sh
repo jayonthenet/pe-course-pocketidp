a07c9b3afab3ca3c7b39ae30b3d595300ca93e27
jayonthenet <clemens@humanitec.com>
2025-01-21 23:16:01 +0100

    Install tools only once. Make restart behaviour more well behaved.
    Signed-off-by: jayonthenet <clemens@humanitec.com>

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,9 +2,15 @@
 sudo apt-get install -y curl unzip wget net-tools jq
 
 # 6. Make certificates happen :)
-curl -JLO "https://dl.filippo.io/mkcert/latest?for=linux/amd64"
-chmod +x mkcert-v*-linux-amd64
-sudo mv mkcert-v*-linux-amd64 /usr/local/bin/mkcert
+if ! command -v mkcert &> /dev/null
+then
+  echo "mkcert not found. Installing..."
+  curl -JLO "https://dl.filippo.io/mkcert/latest?for=linux/amd64"
+  chmod +x mkcert-v*-linux-amd64
+  sudo mv mkcert-v*-linux-amd64 /usr/local/bin/mkcert
+else
+  echo "mkcert is already installed."
+fi
 export CAROOT="/workspaces"
 mkcert -install
 
@@ -27,46 +33,85 @@ else
     echo "Unsupported architecture: $ARCH"
     exit 1
 fi
-VERSION="1.5.7"
-wget "https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_${ARCH}.zip"
-unzip terraform_${VERSION}_linux_${ARCH}.zip
-sudo mv terraform /usr/local/bin/
-rm terraform_${VERSION}_linux_${ARCH}.zip
+
+if ! command -v terraform &> /dev/null
+then
+  echo "Terraform not found. Installing..."
+  VERSION="1.5.7"
+  wget "https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_${ARCH}.zip"
+  unzip terraform_${VERSION}_linux_${ARCH}.zip
+  sudo mv terraform /usr/local/bin/
+  rm terraform_${VERSION}_linux_${ARCH}.zip
+else
+  echo "Terraform is already installed."
+fi
 
 # For YQ
-VERSION="v4.35.1" # Replace with the desired version
-wget "https://github.com/mikefarah/yq/releases/download/${VERSION}/yq_linux_${ARCH}"
-sudo mv yq_linux_${ARCH} /usr/local/bin/yq
+if ! command -v yq &> /dev/null
+then
+  echo "yq not found. Installing..."
+  VERSION="v4.35.1" # Replace with the desired version
+  wget "https://github.com/mikefarah/yq/releases/download/${VERSION}/yq_linux_${ARCH}"
+  sudo mv yq_linux_${ARCH} /usr/local/bin/yq
+else
+  echo "yq is already installed."
+fi
 
 # For score-k8s AMD64 / x86_64
-[ $(uname -m) = x86_64 ] && curl -sLO "https://github.com/score-spec/score-k8s/releases/download/0.1.18/score-k8s_0.1.18_linux_amd64.tar.gz"
-# For score-k8s ARM64
-[ $(uname -m) = aarch64 ] && curl -sLO "https://github.com/score-spec/score-k8s/releases/download/0.1.18/score-k8s_0.1.18_linux_arm64.tar.gz"
-tar xvzf score-k8s*.tar.gz
-rm score-k8s*.tar.gz README.md LICENSE
-sudo mv ./score-k8s /usr/local/bin/score-k8s
-sudo chown root: /usr/local/bin/score-k8s
+if ! command -v score-k8s &> /dev/null
+then
+  echo "score-k8s not found. Installing..."
+  [ $(uname -m) = x86_64 ] && curl -sLO "https://github.com/score-spec/score-k8s/releases/download/0.1.18/score-k8s_0.1.18_linux_amd64.tar.gz"
+  # For score-k8s ARM64
+  [ $(uname -m) = aarch64 ] && curl -sLO "https://github.com/score-spec/score-k8s/releases/download/0.1.18/score-k8s_0.1.18_linux_arm64.tar.gz"
+  tar xvzf score-k8s*.tar.gz
+  rm score-k8s*.tar.gz README.md LICENSE
+  run_as_root mv ./score-k8s /usr/local/bin/score-k8s
+  run_as_root chown root: /usr/local/bin/score-k8s
+else
+  echo "score-k8s is already installed."
+fi
 
-# For Kubectl AMD64 / x86_64
-[ $(uname -m) = x86_64 ] && curl -sLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-# For Kubectl ARM64
-[ $(uname -m) = aarch64 ] && curl -sLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+# Check if kubectl is installed
+if ! command -v kubectl &> /dev/null
+then
+  echo "kubectl not found. Installing..."
+  # For Kubectl AMD64 / x86_64
+  [ $(uname -m) = x86_64 ] && curl -sLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+  # For Kubectl ARM64
+  [ $(uname -m) = aarch64 ] && curl -sLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
+  chmod +x ./kubectl
+  run_as_root mv ./kubectl /usr/local/bin/kubectl
+else
+  echo "kubectl is already installed."
+fi
 
-# For Kind AMD64 / x86_64
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.26.0/kind-linux-amd64
-# For ARM64
-[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.26.0/kind-linux-arm64
-chmod +x ./kind
-sudo mv ./kind /usr/local/bin/kind
+# Check if kind is installed
+if ! command -v kind &> /dev/null
+then
+  echo "kind not found. Installing..."
+  # For Kind AMD64 / x86_64
+  [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.26.0/kind-linux-amd64
+  # For ARM64
+  [ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.26.0/kind-linux-arm64
+  chmod +x ./kind
+  run_as_root mv ./kind /usr/local/bin/kind
+else
+  echo "kind is already installed."
+fi
 
 # setup autocomplete for kubectl
 sudo apt-get update -y && sudo apt-get install bash-completion -y
 mkdir $HOME/.kube
 echo "source <(kubectl completion bash)" >> $HOME/.bashrc
 echo "complete -F __start_kubectl k" >> $HOME/.bashrc
-docker network create -d=bridge -o com.docker.network.bridge.enable_ip_masquerade=true -o com.docker.network.driver.mtu=1500 --subnet fc00:f853:ccd:e793::/64 kind
+
+# Check if the network already exists and create it if it does not
+if ! docker network ls | grep -q 'kind'; then
+  docker network create -d=bridge -o com.docker.network.bridge.enable_ip_masquerade=true -o com.docker.network.driver.mtu=1500 --subnet fc00:f853:ccd:e793::/64 kind
+else
+  echo "Network 'kind' already exists."
+fi
 
 export BASE_DIR=/home/vscode
 mkdir -p $BASE_DIR/state/kube
@@ -132,7 +177,9 @@ data:
 EOF
 
 ## Update /etc/hosts with the kind cluster name
-echo "127.0.0.1 5min-idp-control-plane" | sudo tee -a /etc/hosts
+if ! grep -q "5min-idp-control-plane" /etc/hosts; then
+  echo "127.0.0.1 5min-idp-control-plane" | sudo tee -a /etc/hosts
+fi
 
 ## Prep env
 # Get the gateway API in if we want to work with score-k8s
@@ -154,93 +201,97 @@ export TF_VAR_tls_key_string=$PIDP_KEY
 terraform -chdir=setup/terraform init
 terraform -chdir=setup/terraform apply -auto-approve
 
-# Create Gitea Runner for Actions CI
-RUNNER_TOKEN=""
-while [[ -z $RUNNER_TOKEN ]]; do
-  response=$(curl -k -s -X 'GET' 'https://5min-idp-control-plane/api/v1/admin/runners/registration-token' -H 'accept: application/json' -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==')
-  if [[ $response == *"token"* ]]; then
-    RUNNER_TOKEN=$(echo $response | jq -r '.token')
-  fi
-  sleep 1
-done
+# Check if the gitea_runner container is already running
+if [ "$(docker inspect -f '{{.State.Running}}' gitea_runner 2>/dev/null || true)" != 'true' ]; then
+  # Create Gitea Runner for Actions CI
+  RUNNER_TOKEN=""
+  while [[ -z $RUNNER_TOKEN ]]; do
+    response=$(curl -k -s -X 'GET' 'https://5min-idp-control-plane/api/v1/admin/runners/registration-token' -H 'accept: application/json' -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==')
+    if [[ $response == *"token"* ]]; then
+      RUNNER_TOKEN=$(echo $response | jq -r '.token')
+    fi
+    sleep 1
+  done
 
-# Start Gitea Runner
-docker volume create gitea_runner_data
-docker create \
-  --name gitea_runner \
-  -v gitea_runner_data:/data \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /etc/ssl/certs:/etc/ssl/certs:ro \
-  -v /etc/ca-certificates:/etc/ca-certificates:ro \
-  -e CONFIG_FILE=/config.yaml \
-  -e GITEA_INSTANCE_URL=https://5min-idp-control-plane \
-  -e GITEA_RUNNER_REGISTRATION_TOKEN=$RUNNER_TOKEN \
-  -e GITEA_RUNNER_NAME=local \
-  -e GITEA_RUNNER_LABELS=local \
-  --network kind \
-  gitea/act_runner:latest
-# sed 's|###ca-certficates.crt###|'"$TLS_CA_CERT"'|' setup/gitea/config.yaml > setup/gitea/config.done.yaml
-# docker cp setup/gitea/config.done.yaml gitea_runner:/config.yaml
-docker cp setup/gitea/config.yaml gitea_runner:/config.yaml
-docker start gitea_runner
-
-# Create Gitea org and Backstage clone with configuration
-curl -k -X 'POST' \
-  'https://5min-idp-control-plane/api/v1/orgs' \
-  -H 'accept: application/json' \
-  -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
-  -H 'Content-Type: application/json' \
-  -d '{
-  "repo_admin_change_team_access": true,
-  "username": "5minorg",
-  "visibility": "public"
-}'
-curl -k -X 'POST' \
-  'https://5min-idp-control-plane/api/v1/repos/migrate' \
-  -H 'accept: application/json' \
-  -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
-  -H 'Content-Type: application/json' \
-  -d '{
-  "clone_addr": "https://github.com/humanitec-architecture/backstage.git",
-  "mirror": false,
-  "private": false,
-  "repo_name": "backstage",
-  "repo_owner": "5minorg"
-}'
-curl -k -X 'POST' \
-  'https://5min-idp-control-plane/api/v1/orgs/5minorg/actions/variables/CLOUD_PROVIDER' \
-  -H 'accept: application/json' \
-  -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
-  -H 'Content-Type: application/json' \
-  -d '{
-  "value": "5min"
-}'
-curl -k -X 'POST' \
-  'https://5min-idp-control-plane/api/v1/orgs/5minorg/actions/variables/HUMANITEC_ORG_ID' \
-  -H 'accept: application/json' \
-  -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
-  -H 'Content-Type: application/json' \
-  -d '{
-  "value": "'$HUMANITEC_ORG'"
-}'
-humanitec_app_backstage=$(terraform -chdir=setup/terraform output -raw humanitec_app_backstage)
-curl -k -X 'POST' \
-  'https://5min-idp-control-plane/api/v1/orgs/5minorg/actions/variables/HUMANITEC_APP_ID' \
-  -H 'accept: application/json' \
-  -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
-  -H 'Content-Type: application/json' \
-  -d '{
-  "value": "'$humanitec_app_backstage'"
-}'
-### TODO -> Use from env if present instead of extracting
-curl -k -X 'PUT' \
-  'https://5min-idp-control-plane/api/v1/orgs/5minorg/actions/secrets/HUMANITEC_TOKEN' \
-  -H 'accept: application/json' \
-  -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
-  -H 'Content-Type: application/json' \
-  -d '{
-  "data": "'$TF_VAR_humanitec_token'"
-}'
+  # Start Gitea Runner
+  docker volume create gitea_runner_data
+  docker create \
+    --name gitea_runner \
+    -v gitea_runner_data:/data \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /etc/ssl/certs:/etc/ssl/certs:ro \
+    -v /etc/ca-certificates:/etc/ca-certificates:ro \
+    -e CONFIG_FILE=/config.yaml \
+    -e GITEA_INSTANCE_URL=https://5min-idp-control-plane \
+    -e GITEA_RUNNER_REGISTRATION_TOKEN=$RUNNER_TOKEN \
+    -e GITEA_RUNNER_NAME=local \
+    -e GITEA_RUNNER_LABELS=local \
+    --network kind \
+    gitea/act_runner:latest
+  # sed 's|###ca-certficates.crt###|'"$TLS_CA_CERT"'|' setup/gitea/config.yaml > setup/gitea/config.done.yaml
+  # docker cp setup/gitea/config.done.yaml gitea_runner:/config.yaml
+  docker cp setup/gitea/config.yaml gitea_runner:/config.yaml
+  docker start gitea_runner
+  # Create Gitea org and Backstage clone with configuration
+  curl -k -X 'POST' \
+    'https://5min-idp-control-plane/api/v1/orgs' \
+    -H 'accept: application/json' \
+    -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
+    -H 'Content-Type: application/json' \
+    -d '{
+    "repo_admin_change_team_access": true,
+    "username": "5minorg",
+    "visibility": "public"
+  }'
+  curl -k -X 'POST' \
+    'https://5min-idp-control-plane/api/v1/repos/migrate' \
+    -H 'accept: application/json' \
+    -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
+    -H 'Content-Type: application/json' \
+    -d '{
+    "clone_addr": "https://github.com/humanitec-architecture/backstage.git",
+    "mirror": false,
+    "private": false,
+    "repo_name": "backstage",
+    "repo_owner": "5minorg"
+  }'
+  curl -k -X 'POST' \
+    'https://5min-idp-control-plane/api/v1/orgs/5minorg/actions/variables/CLOUD_PROVIDER' \
+    -H 'accept: application/json' \
+    -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
+    -H 'Content-Type: application/json' \
+    -d '{
+    "value": "5min"
+  }'
+  curl -k -X 'POST' \
+    'https://5min-idp-control-plane/api/v1/orgs/5minorg/actions/variables/HUMANITEC_ORG_ID' \
+    -H 'accept: application/json' \
+    -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
+    -H 'Content-Type: application/json' \
+    -d '{
+    "value": "'$HUMANITEC_ORG'"
+  }'
+  humanitec_app_backstage=$(terraform -chdir=setup/terraform output -raw humanitec_app_backstage)
+  curl -k -X 'POST' \
+    'https://5min-idp-control-plane/api/v1/orgs/5minorg/actions/variables/HUMANITEC_APP_ID' \
+    -H 'accept: application/json' \
+    -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
+    -H 'Content-Type: application/json' \
+    -d '{
+    "value": "'$humanitec_app_backstage'"
+  }'
+  ### TODO -> Use from env if present instead of extracting
+  curl -k -X 'PUT' \
+    'https://5min-idp-control-plane/api/v1/orgs/5minorg/actions/secrets/HUMANITEC_TOKEN' \
+    -H 'accept: application/json' \
+    -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
+    -H 'Content-Type: application/json' \
+    -d '{
+    "data": "'$TF_VAR_humanitec_token'"
+  }'
+else
+  echo "gitea_runner container is already running and gitea configured."
+fi
 
 # Set some nice aliases
 echo "alias k='kubectl'" >> $HOME/.bashrc
