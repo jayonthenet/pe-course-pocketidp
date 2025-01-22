e472fbc192348705a2f02935f2933c84e529953a
jayonthenet <clemens@humanitec.com>
2025-01-22 08:59:31 +0100

    Refactor postCreateCommand.sh to consolidate Gitea organization and repository creation commands for improved clarity and maintainability
    Signed-off-by: jayonthenet <clemens@humanitec.com>

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -242,66 +242,67 @@ if [ "$(docker inspect -f '{{.State.Running}}' gitea_runner 2>/dev/null || true)
   # docker cp setup/gitea/config.done.yaml gitea_runner:/config.yaml
   docker cp setup/gitea/config.yaml gitea_runner:/config.yaml
   docker start gitea_runner
-  # Create Gitea org and Backstage clone with configuration
-  curl -k -X 'POST' \
-    'https://5min-idp-control-plane/api/v1/orgs' \
-    -H 'accept: application/json' \
-    -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
-    -H 'Content-Type: application/json' \
-    -d '{
-    "repo_admin_change_team_access": true,
-    "username": "5minorg",
-    "visibility": "public"
-  }'
-  curl -k -X 'POST' \
-    'https://5min-idp-control-plane/api/v1/repos/migrate' \
-    -H 'accept: application/json' \
-    -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
-    -H 'Content-Type: application/json' \
-    -d '{
-    "clone_addr": "https://github.com/humanitec-architecture/backstage.git",
-    "mirror": false,
-    "private": false,
-    "repo_name": "backstage",
-    "repo_owner": "5minorg"
-  }'
-  curl -k -X 'POST' \
-    'https://5min-idp-control-plane/api/v1/orgs/5minorg/actions/variables/CLOUD_PROVIDER' \
-    -H 'accept: application/json' \
-    -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
-    -H 'Content-Type: application/json' \
-    -d '{
-    "value": "5min"
-  }'
-  curl -k -X 'POST' \
-    'https://5min-idp-control-plane/api/v1/orgs/5minorg/actions/variables/HUMANITEC_ORG_ID' \
-    -H 'accept: application/json' \
-    -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
-    -H 'Content-Type: application/json' \
-    -d '{
-    "value": "'$HUMANITEC_ORG'"
-  }'
-  humanitec_app_backstage=$(terraform -chdir=setup/terraform output -raw humanitec_app_backstage)
-  curl -k -X 'POST' \
-    'https://5min-idp-control-plane/api/v1/orgs/5minorg/actions/variables/HUMANITEC_APP_ID' \
-    -H 'accept: application/json' \
-    -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
-    -H 'Content-Type: application/json' \
-    -d '{
-    "value": "'$humanitec_app_backstage'"
-  }'
-  ### TODO -> Use from env if present instead of extracting
-  curl -k -X 'PUT' \
-    'https://5min-idp-control-plane/api/v1/orgs/5minorg/actions/secrets/HUMANITEC_TOKEN' \
-    -H 'accept: application/json' \
-    -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
-    -H 'Content-Type: application/json' \
-    -d '{
-    "data": "'$TF_VAR_humanitec_token'"
-  }'
 else
   echo "gitea_runner container is already running and gitea configured."
 fi
+
+# Create Gitea org and Backstage clone with configuration
+curl -k -X 'POST' \
+  'https://5min-idp-control-plane/api/v1/orgs' \
+  -H 'accept: application/json' \
+  -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "repo_admin_change_team_access": true,
+  "username": "5minorg",
+  "visibility": "public"
+}'
+curl -k -X 'POST' \
+  'https://5min-idp-control-plane/api/v1/repos/migrate' \
+  -H 'accept: application/json' \
+  -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "clone_addr": "https://github.com/humanitec-architecture/backstage.git",
+  "mirror": false,
+  "private": false,
+  "repo_name": "backstage",
+  "repo_owner": "5minorg"
+}'
+curl -k -X 'POST' \
+  'https://5min-idp-control-plane/api/v1/orgs/5minorg/actions/variables/CLOUD_PROVIDER' \
+  -H 'accept: application/json' \
+  -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "value": "5min"
+}'
+curl -k -X 'POST' \
+  'https://5min-idp-control-plane/api/v1/orgs/5minorg/actions/variables/HUMANITEC_ORG_ID' \
+  -H 'accept: application/json' \
+  -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "value": "'$HUMANITEC_ORG'"
+}'
+humanitec_app_backstage=$(terraform -chdir=setup/terraform output -raw humanitec_app_backstage)
+curl -k -X 'POST' \
+  'https://5min-idp-control-plane/api/v1/orgs/5minorg/actions/variables/HUMANITEC_APP_ID' \
+  -H 'accept: application/json' \
+  -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "value": "'$humanitec_app_backstage'"
+}'
+### TODO -> Use from env if present instead of extracting
+curl -k -X 'PUT' \
+  'https://5min-idp-control-plane/api/v1/orgs/5minorg/actions/secrets/HUMANITEC_TOKEN' \
+  -H 'accept: application/json' \
+  -H 'authorization: Basic NW1pbmFkbWluOjVtaW5hZG1pbg==' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "data": "'$TF_VAR_humanitec_token'"
+}'
 
 # Set some nice aliases
 echo "alias k='kubectl'" >> $HOME/.bashrc
